2cb84fbb6c3b4b54dd0613548eceb14847ba190b
pjfrias <pjfrias@gmail.com>
2024-04-10 18:27:34 -0300

    Initial commit

---
+++ src/trabajopractico6lab1/TrabajoPractico6Lab1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabajopractico6lab1;
+
+public class TrabajoPractico6Lab1 {
+
+    public static void main(String[] args) {
+     
+    }
+
+}
