ac66ca52bee75ae31d1dce043a9b7dd4841c03d3
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-04-14 20:31:15 -0300

    Refactorización vista Detodosa

--- src/trabajopractico6lab1/TrabajoPractico6Lab1.java
+++ src/trabajopractico6lab1/TrabajoPractico6Lab1.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package trabajopractico6lab1;
 
 import Vista.Detodosa;
@@ -13,6 +7,7 @@ public class TrabajoPractico6Lab1 {
     public static void main(String[] args) {
         Detodosa deTodo = new Detodosa();
         deTodo.setVisible(true);
+        deTodo.setLocationRelativeTo(null);
     }
-
+    
 }
